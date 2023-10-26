0dafbdd833cd31e1c90e39aa2e928ea24ddd6891
Tyler Anderson <tjander22@gmail.com>
2023-10-26 06:16:10 -0400

    add in more outline code

---
+++ tests/test_video_link.py
@@ -0,0 +1,8 @@
+import pytest
+
+class TestVideoLink:
+    def test_video_link(self):
+        raise NotImplementedError
+
+    def test_verify_video_link(self):
+        raise NotImplementedError
---
+++ tests/test_youtube_video.py
@@ -0,0 +1,8 @@
+import pytest
+
+class TestYoutubeVideo:
+    def test_youtube_video(self):
+        raise NotImplementedError
+
+    def test_download(self):
+        raise NotImplementedError
