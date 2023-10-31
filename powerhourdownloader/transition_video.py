428fd77cca4a8566e042159940bcc06a1cff225c
Tyler Anderson <tjander22@gmail.com>
2023-10-31 07:50:16 -0400

    clean up some code

--- powerhourdownloader/transition_video.py
+++ powerhourdownloader/transition_video.py
@@ -1,6 +1,10 @@
+import logging
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Optional
+
+from moviepy.editor import CompositeVideoClip, TextClip, VideoFileClip
+
 #from powerhourdownloader.text_video_overlay import TextVideoOverlay
 #from powerhourdownloader.transition import Transition
 
@@ -12,133 +16,35 @@ class TransitionVideo():#Transition):
     audio: Optional[Path] = field(default=None)
 
     def _add_text_to_video(self) -> None:
-        # TODO left off here: see this https://stackoverflow.com/questions/54607447/opencv-how-to-overlay-text-on-video
         # TODO this could be in the parent class
         # Python program to write
         # text on video
-        import cv2
-        video_path = Path(__file__).parent / '..' / 'videos' / 'hello-there.mp4'
-        print(video_path.absolute())
-        print(f'video exits: {video_path.exists()}')
-        from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
+        if self.text is None:
+            # If there is no text to add do not do anything
+            return
 
-        video = VideoFileClip(str(video_path))#.subclip(50,60)
+        video_path = Path(__file__).parent / '..' / 'videos' / 'hello-there.mp4'
+        logging.debug(video_path.absolute())
+        if video_path.exists():
+            logging.debug(f'video exist: {video_path.exists()}')
+        else:
+            logging.error(f'video does not exist: {video_path.exists()}')
+
+        video = VideoFileClip(str(video_path))
 
         # Make the text. Many more options are available.
-        txt_clip = ( TextClip("My Holidays 2013",fontsize=70,color='white')
-                     .set_position('center')
-                     .set_duration(10) )  # TODO duration should be whole clip
+        txt_clip = (
+            # TODO use text video overlay instead
+            TextClip(self.text, fontsize=70, color='white')
+                .set_position('center')  # TODO set this myself with text_video_overlay
+                #.set_duration(10)
+        )
 
-        result = CompositeVideoClip([video, txt_clip]) # Overlay text on video
-        result.write_videofile("myHolidays_edited.mp4",fps=25) # Many options...
+        result = CompositeVideoClip([video, txt_clip])  # Overlay text on video
+        # TODO find out original video fps?
+        # TODO what name to use
+        result.write_videofile(f'text-added-{video_path}', fps=25)  # Many options...
 
-        return
-        # TODO left off here please clean up this code
-
-        tree_video = cv2.VideoCapture(str(video_path))
-
-        print(tree_video)
-
-        fps = tree_video.get(cv2.CAP_PROP_FPS)
-        print(fps)
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        out = cv2.VideoWriter('tyler.avi', fourcc, 20.0, (640,480))
-        # This is from here : https://stackoverflow.com/questions/73663569/python-opencv-how-to-write-text-on-video-output-mp4-empty
-        # TODO having problems getting write out to work
-        try:
-            while(True):
-
-                # Capture frames in the video
-                ret, frame = tree_video.read()
-
-                font = cv2.FONT_HERSHEY_SIMPLEX
-
-                cv2.putText(frame,
-                            'TEXT ON VIDEO',
-                            (50, 50),
-                            font, 1,
-                            (0, 640, 480),
-                            2,
-                            cv2.LINE_4)
-                out.write(frame)
-                cv2.imshow('frame', frame)
-
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-
-
-            #tree_video.release()
-            #out.release()
-
-
-            #cv2.destroyAllWindows()
-        except:
-            print("Video has ended.")
-        tree_video.release()
-        out.release()
-        cv2.destroyAllWindows()
-
-        return
-        frame_ = 0
-
-        while True:
-            ret, frame = tree_video.read()
-            font = cv2.FONT_HERSHEY_SIMPLEX
-
-            #on_video_text = text_update(frame_)
-            on_video_text = 'Hello There'
-            cv2.putText(frame, on_video_text, (50, 50), font, 1, (0, 255, 255), 2, cv2.LINE_4)
-
-            frame_ = frame_ + 1
-
-            cv2.imshow('poem on video', frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-            tree_video.release()
-            cv2.destroyAllWindows()
-
-            if frame_ > 1000000:
-                break
-
-
-        return
-
-
-        cap = cv2.VideoCapture(Path('.') / '..' / 'videos' / 'hello-there.mp4')
-
-        while(True):
-
-            # Capture frames in the video
-            ret, frame = cap.read()
-
-            # describe the type of font
-            # to be used.
-            font = cv2.FONT_HERSHEY_SIMPLEX
-
-            # Use putText() method for
-            # inserting text on video
-            cv2.putText(frame,
-                        'TEXT ON VIDEO',
-                        (50, 50),
-                        font, 1,
-                        (0, 255, 255),
-                        2,
-                        cv2.LINE_4)
-
-            # Display the resulting frame
-            cv2.imshow('video', frame)
-
-            # creating 'q' as the quit
-            # button for the video
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        # release the cap object
-        cap.release()
-        # close all windows
-        cv2.destroyAllWindows()
 
     def _add_audio_to_video(self) -> None:
         raise NotImplementedError
