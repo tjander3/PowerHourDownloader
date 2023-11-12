ee7f33d4f44f83a8f7b1c980ee4c36ad364daa20
Tyler Anderson <tjander22@gmail.com>
2023-11-12 11:24:41 -0500

    add in some text to get the section of html from mt60 we need
    Note I tried xml and dom but was not working due to page not being
    formatted correctly, oh well I will just use find and regex

--- powerhourdownloader/mytube60_parser.py
+++ powerhourdownloader/mytube60_parser.py
@@ -1,5 +1,12 @@
+import urllib.request
+import requests
 from dataclasses import dataclass, field
 from pathlib import Path
+from xml.etree.ElementTree import fromstring
+from lxml import etree
+
+from bs4 import BeautifulSoup
+
 from powerhourdownloader.power_hour import PowerHour
 from powerhourdownloader.power_hour_parser import PowerHourParser
 
@@ -11,12 +18,25 @@ class MyTube60Parser(PowerHourParser):
 
     def _get_webpage(self):
         """Get powerhour webpage from self.link"""
-        # TODO left off here
-        raise NotImplementedError
+        page = urllib.request.urlopen(self.link)
+        webpage_hmtl = page.read()
+        return webpage_hmtl
 
     def parse(self):
         videos = []
-        powerhour_webpage = self._get_webpage()
+        powerhour_webpage = str(self._get_webpage())
+        video_list_xpath = '/html/head/script[18]/text()'
+        start_of_playlist = powerhour_webpage.find('$(document).ready(function(){\\n\\t\\tplayList = [];')
+        end_of_playlist = powerhour_webpage.find("nav = \\\'\\\';\\n\\t\\tfor(var i = 0; i < playList.length; i++){")
+
+        #TODO left off here
+        #playList.push({ videoId : 'ap0mqwvf7H0', start : 0.0, end : 61.02097006484987, title : 'Taking Back Sunday - Cute Without the \"E\" (Cut From the Team)' });
+        # Need to regex the above string
+        # capture videoId, start, end, title
+        # then add to videos!
+
+        print(powerhour_webpage[start_of_playlist:end_of_playlist])
+
         raise NotImplementedError
 
 
