75735fcc1e1a36afb173d88f89ec8d0bfe47df69
Tyler Anderson <tjander22@gmail.com>
2023-12-22 08:01:30 -0500

    start of forms

--- powerhourdownloader/webapp/hello_world.py
+++ powerhourdownloader/webapp/hello_world.py
@@ -33,8 +33,9 @@ $ python -m flask run
 ```
 
 """
+import secrets
 from flask import Flask, render_template, redirect, url_for
-#from flask_bootstrap import Bootstrap5  #TODO fix this
+from flask_bootstrap import Bootstrap  # TODO fix this
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
@@ -42,31 +43,39 @@ from wtforms.validators import DataRequired, Length
 # TODO left off here https://python-adv-web-apps.readthedocs.io/en/latest/flask_forms.html
 
 app = Flask(__name__)
+print(f'{app.template_folder=}')
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+foo = secrets.token_urlsafe(16)
+app.secret_key = foo
+# Bootstrap-Flask requires this line
+bootstrap = Bootstrap(app)
+# Flask-WTF requires this line
+csrf = CSRFProtect(app)
 
-@app.route('/form', methods=['GET', 'POST'])
-def form_hello_world():
-    #names = get_names(ACTORS)
-    names = ["tyler", "lauren"]
-    # you must tell the variable 'form' what you named the class, above
-    # 'form' is the variable name used in this template: index.html
-    form = NameForm()
-    message = ""
-    if form.validate_on_submit():
-        name = form.name.data
-        if name.lower() in names:
-            # empty the form field
-            form.name.data = ""
-            id = get_id(ACTORS, name)
-            # redirect the browser to another route and template
-            return redirect( url_for('actor', id=id) )
-        else:
-            message = "That actor is not in our database."
-    return render_template('index.html', names=names, form=form, message=message)
+# @app.route('/')
+# def hello_world():
+#    return 'Hello, World!'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    messages = [{'title': 'Message One',
+                 'content': 'Message One Content'},
+                {'title': 'Message Two',
+                 'content': 'Message Two Content'}
+                ]
+
+    return render_template('index.html', messages=messages)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    return render_template('create.html')
+
+# TODO left off here: https://www.digitalocean.com/community/tutorials/how-to-use-web-forms-in-a-flask-application
+# TODO left off at step 3
+
 
 class NameForm(FlaskForm):
-    name = StringField('Which actor is your favorite?', validators=[DataRequired(), Length(10, 40)])
+    name = StringField('Which actor is your favorite?',
+                       validators=[DataRequired(), Length(10, 40)])
     submit = SubmitField('Submit')
