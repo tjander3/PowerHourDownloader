256865f59083a3ff32e4681b86f04836f706c400
Tyler Anderson <tjander22@gmail.com>
2024-01-06 19:20:26 -0500

    more stuff

--- powerhourdownloader/webapp/hello_world.py
+++ powerhourdownloader/webapp/hello_world.py
@@ -40,7 +40,7 @@ from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
-from powerhourdownloader.mytube60_parser import example_mytube60_parser_setup
+from powerhourdownloader.mytube60_parser import MyTube60Parser, example_mytube60_parser_setup
 from powerhourdownloader.power_hour_runner import PowerHourRunner
 
 # TODO left off here https://python-adv-web-apps.readthedocs.io/en/latest/flask_forms.html
@@ -88,11 +88,45 @@ def create():
 @app.route('/ph/', methods=('GET', 'POST'))
 def create_power_hour():
     if request.method == 'POST':
-        print('Hello Tyler')
-        power_hour_parser = example_mytube60_parser_setup()
-        power_hour_runner = PowerHourRunner(parser=power_hour_parser)
+        # TODO left off here what do we need next,
+        # options
+        """
+        mytube60link
+        transition / link
+        mp3 only
 
-        power_hour_runner.run()
+        also need to add download option
+        """
+        # TODO verify webpage link
+        # TODO verify webpage link x2
+        webpage_link = request.form['webpage_link']
+        audio_only = 'audio_only' in request.form
+        transition_link = request.form['transition_link']
+        start_time = request.form.get('start_time', None)
+        end_time = request.form.get('end_time', None)
+
+        # Convert start and end times to integers if provided
+        start_time = int(start_time) if start_time else None
+        end_time = int(end_time) if end_time else None
+        if not webpage_link:
+            flash('Webpage link is required!')
+        else:
+            # This is where you would add your logic to use these inputs
+            print("Webpage Link:", webpage_link)
+            print("Audio Only:", audio_only)
+            print("Start Time:", start_time)
+            print("End Time:", end_time)
+            print('Hello Tyler')
+            # TODO transitions not implemented yet
+            mytube60 = MyTube60Parser(link=webpage_link)
+            mytube60.parse()
+            mytube60.power_hour.transitions
+            power_hour_runner = PowerHourRunner(parser=mytube60)
+            power_hour_runner.run()
+
+            power_hour_runner.run()
+            # TODO show status
+            return redirect(url_for('index'))
     return render_template('ph.html')
 
 # TODO left off here: https://www.digitalocean.com/community/tutorials/how-to-use-web-forms-in-a-flask-application
