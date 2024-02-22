177bb00c278ae50e9144c2cb0ac00611ed353f12
Tyler Anderson <tjander22@gmail.com>
2024-02-21 23:11:41 -0500

    add in docsting

--- powerhourdownloader/video_link.py
+++ powerhourdownloader/video_link.py
@@ -8,6 +8,13 @@ class VideoLink:
     video_link: str
 
     def verify_video_link(self) -> bool:
+        """Verify if video link works.
+
+        We use youtube-dl so if youtubedl does not support the website
+        this will make it false.
+
+        We also check to make sure that the video is possible to downlad to avoid downloading bad videos.
+        """
         # Got this from here: <https://stackoverflow.com/questions/61465405/how-to-check-if-a-url-is-valid-that-youtube-dl-supports>
         try:
             youtube_dl.YoutubeDL({'quiet': True}).extract_info(self.video_link, download=False)
