b16f9acaefb1b0bb62ad51c20b2c0feb71936e0a
Tyler Anderson <tjander22@gmail.com>
2024-02-21 23:06:46 -0500

    add in changes to be able to verify if a youtube link works

--- powerhourdownloader/video_link.py
+++ powerhourdownloader/video_link.py
@@ -1,9 +1,27 @@
 from dataclasses import dataclass
 
+import youtube_dl
+
 
 @dataclass
 class VideoLink:
     video_link: str
 
     def verify_video_link(self) -> bool:
-        raise NotImplementedError
+        # Got this from here: <https://stackoverflow.com/questions/61465405/how-to-check-if-a-url-is-valid-that-youtube-dl-supports>
+        try:
+            youtube_dl.YoutubeDL({'quiet': True}).extract_info(self.video_link, download=False)
+            return True
+        except Exception:
+            return False
+
+
+def main():
+    bad_link = 'https://www.youtube.com/watch?v=dpq4gmj-7ys'
+    assert not VideoLink(bad_link).verify_video_link()
+
+    good_link = 'https://www.youtube.com/watch?v=mvVBuG4IOW4'
+    assert VideoLink(good_link).verify_video_link()
+
+if __name__ == '__main__':
+    main()
