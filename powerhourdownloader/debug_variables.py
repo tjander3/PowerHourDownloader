00238c5c6dfacc82aa8fcd6f6ebf635b88f7297a
Tyler Anderson <tjander22@gmail.com>
2024-03-06 21:00:16 -0500

    start of debug for video length

--- powerhourdownloader/debug_variables.py
+++ powerhourdownloader/debug_variables.py
@@ -3,3 +3,9 @@ video_debug = (43, 46)
 debug = True
 #debug = False
 flask_log = False
+
+# The length of the power hour videos
+# If None dont do anything. If an int make them that long.
+# This will be useful to set to 1 or something and then
+# the download process will be much quicker
+main_video_length = 1
