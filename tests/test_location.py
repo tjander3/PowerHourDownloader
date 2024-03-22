64991ccbe8dfccbc69bf5921f415b5ad9ab38f91
Tyler Anderson <tjander22@gmail.com>
2024-03-21 21:15:56 -0400

    add in some test for location

--- tests/test_location.py
+++ tests/test_location.py
@@ -1,5 +1,16 @@
 import pytest
 
+from powerhourdownloader.location import Location
+
 class TestLocation:
-    def test_location(self):
-        raise NotImplementedError
+
+    @pytest.mark.parametrize(
+            'kwargs, expected_results', (
+                ({'x': 3, 'y': 4}, (3, 4)),
+                ({'x': 3, 'y': 4, 'str_loc': 'center'}, (3, 4)),
+                ({'str_loc': 'center'}, 'center'),
+            )
+        )
+    def test_location(self, kwargs, expected_results):
+        loc = Location(**kwargs)
+        assert loc.location == expected_results
