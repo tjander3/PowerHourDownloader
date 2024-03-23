c55c0c6617398480fd355dd065839be9eef9c1e5
Tyler Anderson <tjander22@gmail.com>
2024-03-23 11:03:58 -0400

    add in test for test_video_link

--- tests/test_video_link.py
+++ tests/test_video_link.py
@@ -1,9 +1,21 @@
 import pytest
+from powerhourdownloader.video_link import VideoLink, main
 
-@pytest.mark.skip
 class TestVideoLink:
-    def test_video_link(self):
-        raise NotImplementedError
+    @pytest.mark.parametrize(
+        'link, is_valid', (
+            ('https://www.youtube.com/watch?v=mvVBuG4IOW4', True),
+            ('https://www.youtube.com/watch?v=dpq4gmj-7ys', False),
 
-    def test_verify_video_link(self):
-        raise NotImplementedError
+        )
+    )
+    def test_verify_video_link(self, link, is_valid):
+        assert VideoLink(link).verify_video_link() == is_valid
+
+def test_main():
+    """Test main function in video_link.py
+
+    Note ths function already has two asserts so just
+    calling the function should be enough
+    """
+    main()
